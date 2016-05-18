3b1a90755d68df2c0efa62eec86b2ab0aace596a
vakrilov <alexander.vakrilov@telerik.com>
2016-05-18 17:29:56 +0300

    Cleanup and header

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,27 +3,11 @@ import {Component} from "@angular/core";
 @Component({
     selector: "my-app",
     template: `
-<StackLayout>
-    <Label text="Tap the button" class="title"></Label>
-    
-    <Button text="TAP" (tap)="onTap()"></Button>
-
-    <Label [text]="message" class="message" textWrap="true"></Label>
-</StackLayout>
+    <grid-layout rows="auto auto *">
+        <image src="res://header"></image>
+    </grid-layout>
 `,
 })
 export class AppComponent {
-    public counter: number = 16;
 
-    public get message(): string {
-        if (this.counter > 0) {
-            return this.counter + " taps left";
-        } else {
-            return "Hoorraaay! \nYou are ready to start building!";
-        }
-    }
-    
-    public onTap() {
-        this.counter--;
-    }
 }
