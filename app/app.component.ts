fe9259d8d0980857324399d21230581b8a350202
vakrilov <alexander.vakrilov@telerik.com>
2016-05-18 17:45:01 +0300

    Extract session component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,20 +1,32 @@
-import {Component} from "@angular/core";
+import {Component, Input} from "@angular/core";
 import {Session, SessionsService} from "./session.service";
+
+@Component({
+    selector: 'session',
+    template: `
+        <grid-layout row="1"  columns="auto *" rows="auto auto auto" class="container">
+            <image rowSpan="3" [src]="session.isFav ? 'res://fav_1' : 'res://fav_0'"></image>
+            
+            <Label col="1" row="0" [text]="session.start" class="time"></Label>
+            <Label col="1" row="1" [text]="session.title" class="title"></Label>
+            <Label col="1" row="2" [text]="session.speaker" class="speaker"></Label>              
+        </grid-layout>
+  `
+})
+export class SessionComponent {
+    @Input() session: Session;
+}
+
 
 @Component({
     selector: "my-app",
     providers: [SessionsService],
+    directives: [SessionComponent],
     template: `
     <grid-layout rows="auto auto *">
         <image src="res://header"></image>
         
-        <grid-layout row="1"  columns="auto *" rows="auto auto auto" class="container">
-            <image rowSpan="3" [src]="first.isFav ? 'res://fav_1' : 'res://fav_0'"></image>
-            
-            <Label col="1" row="0" [text]="first.start" class="time"></Label>
-            <Label col="1" row="1" [text]="first.title" class="title"></Label>
-            <Label col="1" row="2" [text]="first.speaker" class="speaker"></Label>              
-        </grid-layout>
+        <session [session]="first"></session>
     </grid-layout>
 `,
 })
