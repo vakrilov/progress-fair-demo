7f405282c172c1d2def1be0ff2e9b19bd3af6cc1
vakrilov <alexander.vakrilov@telerik.com>
2016-05-18 17:35:18 +0300

    Add session service

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,24 @@
 import {Component} from "@angular/core";
+import {Session, SessionsService} from "./session.service";
 
 @Component({
     selector: "my-app",
+    providers: [SessionsService],
     template: `
     <grid-layout rows="auto auto *">
         <image src="res://header"></image>
+        
+        <label row="1" [text]="first.title"></label>
+        
     </grid-layout>
 `,
 })
 export class AppComponent {
+    public sessions: Session[];
+    public first: Session;
 
+    constructor(service: SessionsService) {
+        this.sessions = service.getSessions();
+        this.first = this.sessions[0];
+    }
 }
